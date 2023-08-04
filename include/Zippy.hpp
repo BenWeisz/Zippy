9db54a3da54bfeb32d4270249723901fec0d4fbd
Ben Weisz <bencew99@gmail.com>
2023-08-04 00:19:13 -0700

    Add log functions

--- include/Zippy.hpp
+++ include/Zippy.hpp
@@ -11,6 +11,39 @@
 // libzip is released under a 3-clause BSD license | https://libzip.org/license/
 #include <zip.h>
 
+// ChatGPT
+template <typename... Args>
+void zippy_log_error(Args&&... args) {
+    std::ostringstream oss;
+    ((oss << std::forward<Args>(args)), ...);  // Apply << operator to all arguments
+
+    std::cout << "\033[1;41mERROR:\033[0m " << oss.str() << std::endl;
+}
+
+template <typename... Args>
+void zippy_log_warn(Args&&... args) {
+    std::ostringstream oss;
+    ((oss << std::forward<Args>(args)), ...);  // Apply << operator to all arguments
+
+    std::cout << "\033[1;43mWARNING:\033[0m " << oss.str() << std::endl;
+}
+
+template <typename... Args>
+void zippy_log_success(Args&&... args) {
+    std::ostringstream oss;
+    ((oss << std::forward<Args>(args)), ...);  // Apply << operator to all arguments
+
+    std::cout << "\033[1;42mSUCCESS:\033[0m " << oss.str() << std::endl;
+}
+
+template <typename... Args>
+void zippy_log(Args&&... args) {
+    std::ostringstream oss;
+    ((oss << std::forward<Args>(args)), ...);  // Apply << operator to all arguments
+
+    std::cout << oss.str() << std::endl;
+}
+
 /* Zip up a folder into a .zip file:
     inputPath: A relative path to folder to be zipped up
     outputPath: The name of the output zip
@@ -24,17 +57,17 @@
 bool zippy_up(const std::string& inputPath, const std::string& outputPath) {
     // We will only accept relative paths that don't start with a /
     if (inputPath.starts_with("/") || inputPath.starts_with("\\")) {
-        std::cout << "ERROR: Input path must be a relative path that doesn't start with a \"/\" character." << std::endl;
+        zippy_log_error("Path must be a relative path that doesn't start with a \"/\" character.");
         return false;
     }
     if (outputPath.starts_with("/") || outputPath.starts_with("\\")) {
-        std::cout << "ERROR: Output path must be a relative path that doesn't start with a \"/\" character." << std::endl;
+        zippy_log_error("Output path must be a relative path that doesn't start with a \"/\" character.");
         return false;
     }
 
     // Only accept output files that are .zip
     if (!outputPath.ends_with(".zip")) {
-        std::cout << "ERROR: Output target name must be of type .zip" << std::endl;
+        zippy_log_error("Output target name must be of type .zip");
         return false;
     }
 
@@ -42,12 +75,12 @@ bool zippy_up(const std::string& inputPath, const std::string& outputPath) {
     const std::string zipPath = std::filesystem::current_path() / outputPath;
 
     if (!std::filesystem::exists(dataPath)) {
-        std::cout << "ERROR: The data folder: \"" << dataPath << "\" does not exist" << std::endl;
+        zippy_log_error("The data folder: \"", dataPath, "\" does not exist");
         return false;
     }
 
     if (std::filesystem::exists(zipPath)) {
-        std::cout << "LOG: Removing existing zip file: \"" << zipPath << "\"" << std::endl;
+        zippy_log("Removing existing zip file: \"", zipPath, "\"");
         std::filesystem::remove(zipPath);
     }
 
@@ -60,7 +93,7 @@ bool zippy_up(const std::string& inputPath, const std::string& outputPath) {
     int ignore;
     zip_t* zipFile = zip_open(zipPath.c_str(), ZIP_CREATE, &ignore);
     if (zipFile == nullptr) {
-        std::cout << "ERROR: Failed to create zip file: \"" << zipPath << "\"" << std::endl;
+        zippy_log_error("Failed to create zip file: \"", zipPath, "\"");
         return false;
     }
 
@@ -74,8 +107,8 @@ bool zippy_up(const std::string& inputPath, const std::string& outputPath) {
             zip_source_t* entrySource = zip_source_file(zipFile, entryPath.c_str(), 0, 0);
             if (entrySource == nullptr) {
                 const char* strError = zip_strerror(zipFile);
-                std::cout << "ERROR: Failed to create file source for: \"" << relativeEntryPath << "\"" << std::endl;
-                std::cout << "\t" << strError << std::endl;
+                zippy_log_error("Failed to create file source for: \"", relativeEntryPath, "\"");
+                zippy_log("\t", strError);
 
                 zip_discard(zipFile);
                 return false;
@@ -85,8 +118,8 @@ bool zippy_up(const std::string& inputPath, const std::string& outputPath) {
             status = zip_file_add(zipFile, relativeEntryPath.c_str(), entrySource, ZIP_FL_OVERWRITE);
             if (status < 0) {
                 const char* strError = zip_strerror(zipFile);
-                std::cout << "ERROR: Failed to add file: \"" << relativeEntryPath << "\" to zip file" << std::endl;
-                std::cout << "\t" << strError << std::endl;
+                zippy_log_error("Failed to add file: \"", relativeEntryPath, "\" to zip file");
+                zippy_log("\t", strError);
 
                 zip_discard(zipFile);
                 return false;
@@ -95,8 +128,8 @@ bool zippy_up(const std::string& inputPath, const std::string& outputPath) {
             status = zip_dir_add(zipFile, relativeEntryPath.c_str(), ZIP_FL_ENC_GUESS);
             if (status < 0) {
                 const char* strError = zip_strerror(zipFile);
-                std::cout << "ERROR: Failed to add directory: \"" << relativeEntryPath << "\" to zip file" << std::endl;
-                std::cout << "\t" << strError << std::endl;
+                zippy_log_error("Failed to add directory: \"", relativeEntryPath, "\" to zip file");
+                zippy_log("\t", strError);
 
                 zip_discard(zipFile);
                 return false;
@@ -108,8 +141,8 @@ bool zippy_up(const std::string& inputPath, const std::string& outputPath) {
     int err = zip_close(zipFile);
     if (err != 0) {
         const char* strError = zip_strerror(zipFile);
-        std::cout << "ERROR: Failed to save the zip file: \"" << outputPath << "\"" << std::endl;
-        std::cout << "\t" << strError << std::endl;
+        zippy_log_error("Failed to save the zip file: \"", outputPath, "\"");
+        zippy_log("\t", strError);
 
         zip_discard(zipFile);
         return false;
@@ -129,18 +162,18 @@ bool zippy_up(const std::string& inputPath, const std::string& outputPath) {
 */
 bool zippy_down(const std::string& inputPath) {
     if (!std::filesystem::exists(inputPath)) {
-        std::cout << "ERROR: The zip file: \"" << inputPath << "\" does not exist" << std::endl;
+        zippy_log_error("The zip file: \"", inputPath, "\" does not exist");
         return false;
     }
 
     if (inputPath.starts_with("/") || inputPath.starts_with("\\")) {
-        std::cout << "ERROR: Zip path must be a relative path that doesn't start with a \"/\" character." << std::endl;
+        zippy_log_error("Zip path must be a relative path that doesn't start with a \"/\" character.");
         return false;
     }
 
     // Only accept output files that are .zip
     if (!inputPath.ends_with(".zip")) {
-        std::cout << "ERROR: Zip file must be of type .zip" << std::endl;
+        zippy_log_error("Zip file must be of type .zip");
         return false;
     }
 
@@ -149,13 +182,13 @@ bool zippy_down(const std::string& inputPath) {
     int ignore;
     zip_t* zipFile = zip_open(zipPath.c_str(), ZIP_RDONLY, &ignore);
     if (zipFile == nullptr) {
-        std::cout << "ERROR: Failed to create zip file: \"" << zipPath << "\"" << std::endl;
+        zippy_log_error("Failed to create zip file: \"", zipPath, "\"");
         return false;
     }
 
     zip_int64_t numEntries = zip_get_num_entries(zipFile, ZIP_FL_UNCHANGED);
     if (numEntries < 0) {
-        std::cout << "ERROR: The zip file \"" << zipPath << "\" contains no files" << std::endl;
+        zippy_log_error("The zip file \"", zipPath, "\" contains no files");
         zip_discard(zipFile);
 
         return false;
@@ -165,7 +198,7 @@ bool zippy_down(const std::string& inputPath) {
     const std::string folderPath = zipPath.substr(0, zipPath.length() - 4);
     if (std::filesystem::exists(folderPath)) {
         std::filesystem::remove_all(folderPath);
-        std::cout << "LOG: Old zip output folder: \"" << folderPath << "\" was removed" << std::endl;
+        zippy_log("Old zip output folder: \"", folderPath, "\" was removed");
     }
 
     std::filesystem::create_directory(folderPath);
@@ -177,8 +210,8 @@ bool zippy_down(const std::string& inputPath) {
         const char* fileName = zip_get_name(zipFile, i, ZIP_FL_ENC_GUESS);
         if (fileName == nullptr) {
             const char* strerror = zip_strerror(zipFile);
-            std::cout << "ERROR: Failed to get file name of entry: " << i << " from zip file: \"" << zipPath << "\"" << std::endl;
-            std::cout << "\t" << strerror << std::endl;
+            zippy_log_error("Failed to get file name of entry: ", i, " from zip file: \"", zipPath, "\"");
+            zippy_log("\t", strerror);
             zip_discard(zipFile);
 
             return false;
@@ -187,8 +220,8 @@ bool zippy_down(const std::string& inputPath) {
         zip_file_t* fileEntry = zip_fopen(zipFile, fileName, ZIP_FL_COMPRESSED);
         if (fileEntry == nullptr) {
             const char* strerror = zip_strerror(zipFile);
-            std::cout << "ERROR: Failed to read file entry: " << fileName << " from zip file: \"" << zipPath << "\"" << std::endl;
-            std::cout << "\t" << strerror << std::endl;
+            zippy_log_error("Failed to read file entry: ", fileName, " from zip file: \"", zipPath, "\"");
+            zippy_log("\t", strerror);
             zip_discard(zipFile);
 
             return false;
@@ -204,14 +237,14 @@ bool zippy_down(const std::string& inputPath) {
         else {
             status = zip_file_is_seekable(fileEntry);
             if (status == 0) {
-                std::cout << "ERROR: The file: \"" << fileName << "\" is not seekable, cannot determine file size" << std::endl;
+                zippy_log_error("The file: \"", fileName, "\" is not seekable, cannot determine file size");
                 zip_discard(zipFile);
 
                 return false;
             } else if (status == -1) {
                 const char* strerror = zip_strerror(zipFile);
-                std::cout << "ERROR: The file: \"" << fileName << "\" has an invalid configuration" << std::endl;
-                std::cout << "\t" << strerror << std::endl;
+                zippy_log_error("The file: \"", fileName, "\" has an invalid configuration");
+                zippy_log("\t", strerror);
                 zip_discard(zipFile);
 
                 return false;
@@ -221,8 +254,8 @@ bool zippy_down(const std::string& inputPath) {
             status = (int)zip_fseek(fileEntry, 0, SEEK_END);
             if (status < 0) {
                 const char* strerror = zip_strerror(zipFile);
-                std::cout << "ERROR: The file: \"" << fileName << "\" could not be fseek-ed, cannot determine file size" << std::endl;
-                std::cout << "\t" << strerror << std::endl;
+                zippy_log_error("The file: \"", fileName, "\" could not be fseek-ed, cannot determine file size");
+                zippy_log("\t", strerror);
                 zip_discard(zipFile);
 
                 return false;
@@ -232,8 +265,8 @@ bool zippy_down(const std::string& inputPath) {
             int fileSize = zip_ftell(fileEntry);
             if (fileSize < 0) {
                 const char* strerror = zip_strerror(zipFile);
-                std::cout << "ERROR: The file: \"" << fileName << "\" could not be ftell-ed, cannot determine file size" << std::endl;
-                std::cout << "\t" << strerror << std::endl;
+                zippy_log_error("The file: \"", fileName, "\" could not be ftell-ed, cannot determine file size");
+                zippy_log("\t", strerror);
 
                 zip_discard(zipFile);
 
@@ -244,8 +277,8 @@ bool zippy_down(const std::string& inputPath) {
             status = (int)zip_fseek(fileEntry, 0, SEEK_SET);
             if (status < 0) {
                 const char* strerror = zip_strerror(zipFile);
-                std::cout << "ERROR: The file: \"" << fileName << "\" could not be fseek-ed, cannot determine file size" << std::endl;
-                std::cout << "\t" << strerror << std::endl;
+                zippy_log_error("The file: \"", fileName, "\" could not be fseek-ed, cannot determine file size");
+                zippy_log("\t", strerror);
                 zip_discard(zipFile);
 
                 return false;
@@ -254,7 +287,7 @@ bool zippy_down(const std::string& inputPath) {
             // Set up the output stream for the new zip file
             std::ofstream outputStream(folderPath + entryNameStr);
             if (outputStream.fail()) {
-                std::cout << "ERROR: Failed to create new file for: \"" << fileName << "\"" << std::endl;
+                zippy_log_error("Failed to create new file for: \"", fileName, "\"");
                 zip_discard(zipFile);
 
                 return false;
@@ -265,8 +298,8 @@ bool zippy_down(const std::string& inputPath) {
             zip_int64_t readStatus = zip_fread(fileEntry, buf, fileSize);
             if (readStatus != fileSize) {
                 const char* strerror = zip_strerror(zipFile);
-                std::cout << "ERROR: Failed to read data from the file: \"" << fileName << "\"" << std::endl;
-                std::cout << "\t" << strerror << std::endl;
+                zippy_log_error("Failed to read data from the file: \"", fileName, "\"");
+                zippy_log("\t", strerror);
 
                 zip_discard(zipFile);
                 outputStream.close();
